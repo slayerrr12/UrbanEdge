6572e4055269d40c4b89aa8898153c191d7edd12
slayerrr12 <eyeamarinsharma@gmail.com>
2023-06-24 16:54:07 +0530

    added the firebase config data to my env file

--- src/FireBase/firebase.utils.js
+++ src/FireBase/firebase.utils.js
@@ -3,14 +3,15 @@ import { getAuth, GoogleAuthProvider, signInWithPopup } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
+
 const firebaseConfig = {
-  apiKey: "AIzaSyCm-mmTCxnu8fIzA8iexSbhCU2wpadqtYw",
-  authDomain: "urbanedge-10115.firebaseapp.com",
-  projectId: "urbanedge-10115",
-  storageBucket: "urbanedge-10115.appspot.com",
-  messagingSenderId: "712502366969",
-  appId: "1:712502366969:web:92a78b1de539f7e4698828",
-  measurementId: "G-YQRKW9YFL5",
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
 };
 
 const app = initializeApp(firebaseConfig);
